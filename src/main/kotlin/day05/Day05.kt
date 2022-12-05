9dcdefd82cf4d1a141326498b7b274f97d53c806
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-04 20:46:59 -0800

    Prep day 5

---
+++ src/main/kotlin/day05/Day05.kt
@@ -0,0 +1,16 @@
+package day05
+
+import readResourceAsBufferedReader
+
+fun main() {
+    println("part 1: ${part1(readResourceAsBufferedReader("5_1.txt").readLines())}")
+    println("part 2: ${part2(readResourceAsBufferedReader("5_1.txt").readLines())}")
+}
+
+fun part1(input: List<String>): Int {
+    TODO("wait for day 5")
+}
+
+fun part2(input: List<String>): Int {
+    TODO("wait for day 5")
+}
---
+++ src/test/kotlin/day05/Day05Test.kt
@@ -0,0 +1,27 @@
+package day05
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day05Test {
+
+    @Test
+    fun testPart1() {
+        val input = """
+            
+        """.trimIndent()
+            .lines()
+
+        assertEquals(2, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+        """.trimIndent()
+            .lines()
+
+        assertEquals(4, part2(input))
+    }
+
+}
