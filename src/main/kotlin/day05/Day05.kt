ba7224ae1881322e97490063785f0ba65f5015ef
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-04 22:08:04 -0800

    Day 5 done

--- src/main/kotlin/day05/Day05.kt
+++ src/main/kotlin/day05/Day05.kt
@@ -1,5 +1,6 @@
 package day05
 
+import byEmptyLines
 import readResourceAsBufferedReader
 
 fun main() {
@@ -7,10 +8,87 @@ fun main() {
     println("part 2: ${part2(readResourceAsBufferedReader("5_1.txt").readLines())}")
 }
 
-fun part1(input: List<String>): Int {
-    TODO("wait for day 5")
+fun part1(input: List<String>): String {
+    return solve(input) { stack, instr -> stack.execute1(instr) }
 }
 
-fun part2(input: List<String>): Int {
-    TODO("wait for day 5")
+fun part2(input: List<String>): String {
+    return solve(input) { stack, instr -> stack.execute2(instr) }
 }
+
+private fun solve(input: List<String>, stackFn: (SupplyStacks, CraneInstruction) -> Unit): String {
+    val (stack, instructions) = parseInput(input)
+
+    instructions.forEach { stackFn.invoke(stack, it) }
+
+    return stack.tops()
+}
+
+fun parseInput(input: List<String>): Pair<SupplyStacks, List<CraneInstruction>> {
+    val (stacks, instructions) = input.byEmptyLines()
+
+    val stack = parseSupplyStack(stacks.lines())
+    val instrs = parseInstructions(instructions.lines())
+
+    return stack to instrs
+}
+
+fun parseSupplyStack(input: List<String>): SupplyStacks {
+    val base = input.last()
+        .chunked(4)
+
+    val stacks = List(base.size) { StringBuilder() }
+
+    for (i in (input.size - 2) downTo 0) {
+        val row = input[i]
+        val blocks = row.chunked(4)
+
+        blocks.forEachIndexed { idx, block ->
+            if (block.isNotBlank()) {
+                stacks[idx].append(block[1])
+            }
+        }
+    }
+
+    return SupplyStacks(stacks)
+}
+
+fun parseInstructions(input: List<String>): List<CraneInstruction> {
+    return input.map { parseInstruction(it) }
+}
+
+fun parseInstruction(line: String): CraneInstruction {
+    val parts = line.split(" ")
+    return CraneInstruction(
+        parts[1].toInt(),
+        parts[3].toInt(),
+        parts[5].toInt()
+    )
+}
+
+class SupplyStacks(private val stacks: List<StringBuilder>) {
+
+    fun execute1(instr: CraneInstruction) {
+        execute(instr) { it.reversed() }
+    }
+
+    fun execute2(instr: CraneInstruction) {
+        execute(instr) { it }
+    }
+
+    private fun execute(instr: CraneInstruction, moveFn: (String) -> String) {
+        val src = stacks[instr.src - 1]
+        val move = src.substring(src.length - instr.amt)
+        src.setLength(src.length - instr.amt)
+
+        val dst = stacks[instr.dst - 1]
+        dst.append(moveFn.invoke(move))
+    }
+
+    fun tops(): String {
+        return stacks.joinToString("") { it.last().toString() }
+    }
+
+}
+
+data class CraneInstruction(val amt: Int, val src: Int, val dst: Int)
--- src/test/kotlin/day05/Day05Test.kt
+++ src/test/kotlin/day05/Day05Test.kt
@@ -8,20 +8,37 @@ class Day05Test {
     @Test
     fun testPart1() {
         val input = """
+                [D]    
+            [N] [C]    
+            [Z] [M] [P]
+             1   2   3 
             
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
         """.trimIndent()
             .lines()
 
-        assertEquals(2, part1(input))
+        assertEquals("CMZ", part1(input))
     }
 
     @Test
     fun testPart2() {
         val input = """
+                [D]    
+            [N] [C]    
+            [Z] [M] [P]
+             1   2   3 
+            
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
         """.trimIndent()
             .lines()
 
-        assertEquals(4, part2(input))
+        assertEquals("MCD", part2(input))
     }
 
 }
