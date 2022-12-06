bf8065fabb316b26cdc4aa2e753105025e62c864
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-05 21:59:16 -0800

    Day 6 done

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -3,14 +3,45 @@ package day06
 import readResourceAsBufferedReader
 
 fun main() {
-    println("part 1: ${part1(readResourceAsBufferedReader("6_1.txt").readLines())}")
-    println("part 2: ${part2(readResourceAsBufferedReader("6_1.txt").readLines())}")
+    println("part 1: ${part1(readResourceAsBufferedReader("6_1.txt").readLine())}")
+    println("part 2: ${part2(readResourceAsBufferedReader("6_1.txt").readLine())}")
 }
 
-fun part1(input: List<String>): String {
-    TODO()
+fun part1(input: String): Int {
+    return packetize(4, input).first()
 }
 
-fun part2(input: List<String>): String {
-    TODO()
+fun part2(input: String): Int {
+    return packetize(14, input).first()
 }
+
+fun packetize(unique: Int, input: String): Sequence<Int> = sequence {
+    var l = 0
+    var r = 0
+
+    while (r < input.length) {
+        val uniqueChars = mutableSetOf<Char>()
+
+        while (uniqueChars.size < unique && r < input.length) {
+            val test = input[r]
+            if (!uniqueChars.contains(test)) {
+                uniqueChars.add(test)
+                r++
+            } else {
+                while (l < r) {
+                    if (input[l] != test) {
+                        uniqueChars.remove(input[l])
+                        l++
+                    } else {
+                        l++
+                        break
+                    }
+                }
+                r++
+            }
+        }
+        yield(r)
+        l = r
+    }
+}
+
--- src/test/kotlin/day06/Day06Test.kt
+++ src/test/kotlin/day06/Day06Test.kt
@@ -1,44 +1,46 @@
 package day06
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertEquals
 
 class Day06Test {
 
-    @Test
-    fun testPart1() {
-        val input = """
-                [D]    
-            [N] [C]    
-            [Z] [M] [P]
-             1   2   3 
-            
-            move 1 from 2 to 1
-            move 3 from 1 to 3
-            move 2 from 2 to 1
-            move 1 from 1 to 2
-        """.trimIndent()
-            .lines()
-
-        assertEquals("CMZ", part1(input))
+    @ParameterizedTest(name = "Verify part1({0}) is {1}")
+    @MethodSource("part1")
+    fun testPart1(input: String, expected: Int) {
+        assertEquals(expected, part1(input))
     }
 
-    @Test
-    fun testPart2() {
-        val input = """
-                [D]    
-            [N] [C]    
-            [Z] [M] [P]
-             1   2   3 
-            
-            move 1 from 2 to 1
-            move 3 from 1 to 3
-            move 2 from 2 to 1
-            move 1 from 1 to 2
-        """.trimIndent()
-            .lines()
+    @ParameterizedTest(name = "Verify part2({0}) is {1}")
+    @MethodSource("part2")
+    fun testPart2(input: String, expected: Int) {
+        assertEquals(expected, part2(input))
+    }
 
-        assertEquals("MCD", part2(input))
+    companion object {
+        @JvmStatic
+        fun part1(): List<Arguments> {
+            return listOf(
+                Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
+                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 6),
+                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)
+            )
+        }
+
+        @JvmStatic
+        fun part2(): List<Arguments> {
+            return listOf(
+                Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 23),
+                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)
+            )
+        }
     }
 
 }
