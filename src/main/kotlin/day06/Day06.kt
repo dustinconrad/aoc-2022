f4a124cc40fcfd58d3d7ff82ea033c08f7696ba2
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-05 20:50:19 -0800

    Prep day 6

---
+++ src/main/kotlin/day06/Day06.kt
@@ -0,0 +1,16 @@
+package day06
+
+import readResourceAsBufferedReader
+
+fun main() {
+    println("part 1: ${part1(readResourceAsBufferedReader("6_1.txt").readLines())}")
+    println("part 2: ${part2(readResourceAsBufferedReader("6_1.txt").readLines())}")
+}
+
+fun part1(input: List<String>): String {
+    TODO()
+}
+
+fun part2(input: List<String>): String {
+    TODO()
+}
