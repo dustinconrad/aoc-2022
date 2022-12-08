2abf1141ba27bbdbf3e445087492f3c434df1bd3
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-07 20:24:03 -0800

    Prep day 8

---
+++ src/main/kotlin/day08/Day08.kt
@@ -0,0 +1,17 @@
+package day08
+
+import readResourceAsBufferedReader
+import java.util.function.Predicate
+
+fun main() {
+    println("part 1: ${part1(readResourceAsBufferedReader("8_1.txt").readLines())}")
+    println("part 2: ${part2(readResourceAsBufferedReader("8_1.txt").readLines())}")
+}
+
+fun part1(input: List<String>): Int {
+    TODO()
+}
+
+fun part2(input: List<String>): Int {
+    TODO()
+}
---
+++ src/test/kotlin/day08/Day08Test.kt
@@ -0,0 +1,72 @@
+package day08
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day08Test {
+
+    @Test
+    fun testPart1() {
+        val input = """
+            ${'$'} cd /
+            ${'$'} ls
+            dir a
+            14848514 b.txt
+            8504156 c.dat
+            dir d
+            ${'$'} cd a
+            ${'$'} ls
+            dir e
+            29116 f
+            2557 g
+            62596 h.lst
+            ${'$'} cd e
+            ${'$'} ls
+            584 i
+            ${'$'} cd ..
+            ${'$'} cd ..
+            ${'$'} cd d
+            ${'$'} ls
+            4060174 j
+            8033020 d.log
+            5626152 d.ext
+            7214296 k
+        """.trimIndent()
+            .lines()
+
+        assertEquals(95437, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+            ${'$'} cd /
+            ${'$'} ls
+            dir a
+            14848514 b.txt
+            8504156 c.dat
+            dir d
+            ${'$'} cd a
+            ${'$'} ls
+            dir e
+            29116 f
+            2557 g
+            62596 h.lst
+            ${'$'} cd e
+            ${'$'} ls
+            584 i
+            ${'$'} cd ..
+            ${'$'} cd ..
+            ${'$'} cd d
+            ${'$'} ls
+            4060174 j
+            8033020 d.log
+            5626152 d.ext
+            7214296 k
+        """.trimIndent()
+            .lines()
+
+        assertEquals(24933642, part2(input))
+    }
+
+}
