b68eb7bcce772ab75e02b31e87b4df3db9c3b156
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-07 22:36:04 -0800

    Day 8 done, not pretty

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -1,7 +1,6 @@
 package day08
 
 import readResourceAsBufferedReader
-import java.util.function.Predicate
 
 fun main() {
     println("part 1: ${part1(readResourceAsBufferedReader("8_1.txt").readLines())}")
@@ -9,9 +8,133 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    TODO()
+    val grid = parseGrid(input)
+    return visible(grid).size
 }
 
 fun part2(input: List<String>): Int {
-    TODO()
+    val grid = parseGrid(input)
+    val scores = scenicScore(grid)
+    return scores.maxOfOrNull { it.max() } ?: throw IllegalStateException()
+}
+
+fun parseGrid(input: List<String>): List<List<Int>> {
+    return input.map { line -> line.toCharArray().toList().map { it.code - '0'.code } }
+}
+
+fun visible(grid: List<List<Int>>): Set<Pair<Int, Int>> {
+    val answer = mutableSetOf<Pair<Int,Int>>()
+
+    for (y in grid.indices) {
+
+        // left to right
+        val row = grid[y]
+        var tallestSoFar = -1
+        for (x in row.indices) {
+            if (row[x] > tallestSoFar) {
+                answer.add(y to x)
+                tallestSoFar = row[x]
+            }
+        }
+
+        // right to left
+        tallestSoFar = -1
+        for (x in row.indices.reversed()) {
+            if (row[x] > tallestSoFar) {
+                answer.add(y to x)
+                tallestSoFar = row[x]
+            }
+        }
+    }
+
+    for (x in 0 until grid[0].size) {
+
+        // top to bottom
+        var tallestSoFar = -1
+        for (y in grid.indices) {
+            if (grid[y][x] > tallestSoFar) {
+                answer.add(y to x)
+                tallestSoFar = grid[y][x]
+            }
+        }
+        // bottom to top
+        tallestSoFar = -1
+        for (y in grid.indices.reversed()) {
+            if (grid[y][x] > tallestSoFar) {
+                answer.add(y to x)
+                tallestSoFar = grid[y][x]
+            }
+        }
+    }
+
+    return answer
+}
+fun scenicScore(grid: List<List<Int>>): Array<IntArray> {
+
+    fun right(y: Int, x: Int): Int {
+        val curr = grid[y][x]
+        var answer = 0
+        var i = x
+        while(i < grid[y].size - 1) {
+            answer++
+            if (grid[y][++i] >= curr) {
+                break
+            }
+        }
+        return answer
+    }
+
+    fun left(y: Int, x: Int): Int {
+        val curr = grid[y][x]
+        var answer = 0
+        var i = x
+        while(i > 0) {
+            answer++
+            if (grid[y][--i] >= curr) {
+                break
+            }
+        }
+        return answer
+    }
+
+    fun down(y: Int, x: Int): Int {
+        val curr = grid[y][x]
+        var answer = 0
+        var i = y
+        while(i < grid.size - 1) {
+            answer++
+            if (grid[++i][x] >= curr) {
+                break
+            }
+        }
+        return answer
+    }
+
+    fun up(y: Int, x: Int): Int {
+        val curr = grid[y][x]
+        var answer = 0
+        var i = y
+        while(i > 0) {
+            answer++
+            if (grid[--i][x] >= curr) {
+                break
+            }
+        }
+        return answer
+    }
+
+    val scores = Array(grid.size) { IntArray(grid[0].size) }
+
+    for (y in 1 until grid.size - 1) {
+        val row = grid[y]
+        for (x in 1 until row.size - 1) {
+            val up = up(y, x)
+            val left = left(y, x)
+            val right = right(y, x)
+            val down = down(y, x)
+
+            scores[y][x] = right * left * down * up
+        }
+    }
+    return scores
 }
--- src/test/kotlin/day08/Day08Test.kt
+++ src/test/kotlin/day08/Day08Test.kt
@@ -8,65 +8,29 @@ class Day08Test {
     @Test
     fun testPart1() {
         val input = """
-            ${'$'} cd /
-            ${'$'} ls
-            dir a
-            14848514 b.txt
-            8504156 c.dat
-            dir d
-            ${'$'} cd a
-            ${'$'} ls
-            dir e
-            29116 f
-            2557 g
-            62596 h.lst
-            ${'$'} cd e
-            ${'$'} ls
-            584 i
-            ${'$'} cd ..
-            ${'$'} cd ..
-            ${'$'} cd d
-            ${'$'} ls
-            4060174 j
-            8033020 d.log
-            5626152 d.ext
-            7214296 k
+            30373
+            25512
+            65332
+            33549
+            35390
         """.trimIndent()
             .lines()
 
-        assertEquals(95437, part1(input))
+        assertEquals(21, part1(input))
     }
 
     @Test
     fun testPart2() {
         val input = """
-            ${'$'} cd /
-            ${'$'} ls
-            dir a
-            14848514 b.txt
-            8504156 c.dat
-            dir d
-            ${'$'} cd a
-            ${'$'} ls
-            dir e
-            29116 f
-            2557 g
-            62596 h.lst
-            ${'$'} cd e
-            ${'$'} ls
-            584 i
-            ${'$'} cd ..
-            ${'$'} cd ..
-            ${'$'} cd d
-            ${'$'} ls
-            4060174 j
-            8033020 d.log
-            5626152 d.ext
-            7214296 k
+            30373
+            25512
+            65332
+            33549
+            35390
         """.trimIndent()
             .lines()
 
-        assertEquals(24933642, part2(input))
+        assertEquals(8, part2(input))
     }
 
 }
