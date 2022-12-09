976c7b2a77130a8335c44e5a61ddf8537239d9be
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-08 20:29:02 -0800

    Day 9 prep

---
+++ src/test/kotlin/day09/Day09Test.kt
@@ -0,0 +1,36 @@
+package day09
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day09Test {
+
+    @Test
+    fun testPart1() {
+        val input = """
+            30373
+            25512
+            65332
+            33549
+            35390
+        """.trimIndent()
+            .lines()
+
+        assertEquals(21, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = """
+            30373
+            25512
+            65332
+            33549
+            35390
+        """.trimIndent()
+            .lines()
+
+        assertEquals(8, part2(input))
+    }
+
+}
