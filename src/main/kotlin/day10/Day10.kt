2fd4cac4bba8df6c8770021b0d2f8edf6651b48e
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-09 22:48:59 -0800

    Day 10 done

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,18 +1,84 @@
 package day10
 
-import day09.Dir
-import day09.RopeBridge
 import readResourceAsBufferedReader
 
 fun main() {
     println("part 1: ${part1(readResourceAsBufferedReader("10_1.txt").readLines())}")
-    println("part 2: ${part2(readResourceAsBufferedReader("10_1.txt").readLines())}")
+    println("part 2: \n${part2(readResourceAsBufferedReader("10_1.txt").readLines())}")
 }
 
 fun part1(input: List<String>): Int {
-    TODO()
+    val instrs = input.map { Instruction.parse(it) }
+    val signalStrength = signalStrength(instrs)
+
+    return listOf(20, 60, 100, 140, 180, 220)
+        .map { signalStrength[it - 1] }
+        .sum()
 }
 
-fun part2(input: List<String>): Int {
-    TODO()
+fun part2(input: List<String>): String {
+    val instrs = input.map { Instruction.parse(it) }
+    val spritePos = runResults(instrs)
+
+    return spritePos.mapIndexed { cycle, pos ->
+        val cycleX = cycle % 40
+        if ((pos - 1..pos + 1).contains(cycleX)) {
+            "#"
+        } else {
+            "."
+        }
+    }.joinToString("")
+        .chunked(40)
+        .joinToString("\n")
+}
+
+sealed class Instruction(val cycles: Int) {
+    abstract fun complete(cpu: Cpu)
+
+    companion object {
+        fun parse(line: String): Instruction {
+            return if (line == "noop") {
+                NoOp
+            } else {
+                val (_, v) = line.split(" ")
+                AddX(v.toInt())
+            }
+        }
+    }
+}
+
+data class AddX(val value: Int): Instruction(2) {
+    override fun complete(cpu: Cpu) {
+        cpu.register += value
+    }
+
+}
+
+object NoOp : Instruction(1) {
+    override fun complete(cpu: Cpu) {
+        //do nothing
+    }
+}
+
+fun runResults(instrs: List<Instruction>): List<Int> {
+    val cpu = Cpu()
+    return instrs.flatMap { cpu.step(it) }
+}
+
+fun signalStrength(instrs: List<Instruction>): List<Int> {
+    val cpu = Cpu()
+    return instrs.flatMap { cpu.step(it) }
+        .mapIndexed { idx, v -> (idx + 1) * v }
+}
+
+class Cpu {
+
+    var register = 1
+
+    fun step(instr: Instruction): List<Int> {
+        val currX = register
+        instr.complete(this)
+        return MutableList(instr.cycles) { currX }
+    }
+
 }
--- src/test/kotlin/day10/Day10Test.kt
+++ src/test/kotlin/day10/Day10Test.kt
@@ -6,54 +6,512 @@ import kotlin.test.assertEquals
 class Day10Test {
 
     @Test
-    fun testPart1() {
+    fun testPart1_1() {
         val input = """
-            R 4
-            U 4
-            L 3
-            D 1
-            R 4
-            D 1
-            L 5
-            R 2
+            noop
+            addx 3
+            addx -5
         """.trimIndent()
             .lines()
 
-        assertEquals(13, part1(input))
+        val instructions = input.map { Instruction.parse(it) }
+
+        val result = runResults(instructions)
+
+        assertEquals(listOf(1, 1, 1, 4, 4), result)
     }
 
     @Test
-    fun testPart2() {
+    fun testPart1_2() {
         val input = """
-            R 4
-            U 4
-            L 3
-            D 1
-            R 4
-            D 1
-            L 5
-            R 2
+            addx 15
+            addx -11
+            addx 6
+            addx -3
+            addx 5
+            addx -1
+            addx -8
+            addx 13
+            addx 4
+            noop
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop
         """.trimIndent()
             .lines()
 
-        assertEquals(1, part2(input))
+        val instructions = input.map { Instruction.parse(it) }
+
+        val result = runResults(instructions)
+
+        val subResults = result.subList(179, 220)
+        println(result.subList(0, 21))
+
+        assertEquals(21, result[19])
+        assertEquals(19, result[59])
+        assertEquals(18, result[99])
+        assertEquals(21, result[139])
+        assertEquals(16, result[179])
+        assertEquals(18, result[219])
+    }
+
+    @Test
+    fun testPart1_3() {
+        val input = """
+            addx 15
+            addx -11
+            addx 6
+            addx -3
+            addx 5
+            addx -1
+            addx -8
+            addx 13
+            addx 4
+            noop
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop
+        """.trimIndent()
+            .lines()
+
+        val instructions = input.map { Instruction.parse(it) }
+
+        val result = signalStrength(instructions)
+
+        assertEquals(420, result[19])
+        assertEquals(1140, result[59])
+        assertEquals(1800, result[99])
+        assertEquals(2940, result[139])
+        assertEquals(2880, result[179])
+        assertEquals(3960, result[219])
     }
 
     @Test
     fun testPart2part2() {
         val input = """
-            R 5
-            U 8
-            L 8
-            D 3
-            R 17
-            D 10
-            L 25
-            U 20
+            addx 15
+            addx -11
+            addx 6
+            addx -3
+            addx 5
+            addx -1
+            addx -8
+            addx 13
+            addx 4
+            noop
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop
         """.trimIndent()
             .lines()
 
-        assertEquals(36, part2(input))
+        assertEquals(
+"##..##..##..##..##..##..##..##..##..##..\n" +
+        "###...###...###...###...###...###...###.\n" +
+        "####....####....####....####....####....\n" +
+        "#####.....#####.....#####.....#####.....\n" +
+        "######......######......######......####\n" +
+        "#######.......#######.......#######....."
+            , part2(input))
     }
 
 }
