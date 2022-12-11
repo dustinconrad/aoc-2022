b42829098d6e743a613fe702092fef4d6e6461c4
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-10 23:02:27 -0800

    Part 2 done

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -2,40 +2,45 @@ package day11
 
 import byEmptyLines
 import readResourceAsBufferedReader
+import java.math.BigInteger
 import java.util.function.BinaryOperator
 
 fun main() {
     println("part 1: ${part1(readResourceAsBufferedReader("11_1.txt").readLines())}")
-    //println("part 2: \n${part2(readResourceAsBufferedReader("10_1.txt").readLines())}")
+    println("part 2: ${part2(readResourceAsBufferedReader("11_1.txt").readLines())}")
 }
 
-fun part1(input: List<String>): Int {
+fun part1(input: List<String>): Long {
     val monkeys = input.byEmptyLines().map { parseMonkey(it) }
     val middle = MonkeyInTheMiddle(monkeys)
     repeat(20) { middle.round() }
 
-    val sorted = middle.monkeys.map { it.inspectCount }.sortedDescending()
-    println(sorted)
+    val sorted = middle.monkeys.map { it.inspectCount.toLong() }.sortedDescending()
     return sorted[0] * sorted[1]
 }
 
-fun part2(input: List<String>): String {
-    TODO()
+fun part2(input: List<String>): Long {
+    val monkeys = input.byEmptyLines().map { parseMonkey(it) }
+    val middle = MonkeyInTheMiddle(monkeys)
+    repeat(10_000) { middle.round2() }
+
+    val sorted = middle.monkeys.map { it.inspectCount.toLong() }.sortedDescending()
+    return sorted[0] * sorted[1]
 }
 
-data class Operation(val l: String, val op: BinaryOperator<Int>, val r: String) {
+data class Operation(val l: String, val op: BinaryOperator<BigInteger>, val r: String) {
 
-    fun call(worry: Int): Int {
+    fun call(worry: BigInteger): BigInteger {
         val left = if (l == "old") {
             worry
         } else {
-            l.toInt()
+            l.toBigInteger()
         }
 
         val right = if (r == "old") {
             worry
         } else {
-            r.toInt()
+            r.toBigInteger()
         }
 
         return op.apply(left, right)
@@ -46,10 +51,10 @@ data class Operation(val l: String, val op: BinaryOperator<Int>, val r: String) 
             val parts = line.trim().split(Regex("\\s+"))
             val l = parts[3]
             val r = parts[5]
-            val op: (Int, Int) -> Int = when(parts[4]) {
-                "*" -> { a, b -> a * b }
-                "+" -> { a, b -> a + b }
-                "-" -> { a, b -> a - b }
+            val op: (BigInteger, BigInteger) -> BigInteger = when(parts[4]) {
+                "*" -> { a, b -> a.times(b) }
+                "+" -> { a, b -> a.plus(b)}
+                "-" -> { a, b -> a.minus(b) }
                 else -> throw IllegalArgumentException("Unknown op: ${parts[4]}")
             }
             return Operation(l, op, r)
@@ -60,9 +65,9 @@ data class Operation(val l: String, val op: BinaryOperator<Int>, val r: String) 
 fun parseMonkey(monkey: String): Monkey {
     val lines = monkey.lines()
     val id = lines[0].trim().replace(":","", false).split(Regex("\\s+"))[1].toInt()
-    val start = lines[1].split(":")[1].split(",").map { it.trim() }.map { it.toInt() }.toMutableList()
+    val start = lines[1].split(":")[1].split(",").map { it.trim() }.map { it.toBigInteger() }.toMutableList()
     val op = Operation.parse(lines[2])
-    val test = lines[3].trim().split(Regex("\\s+")).last().toInt()
+    val test = lines[3].trim().split(Regex("\\s+")).last().toBigInteger()
     val tr = lines[4].trim().split(Regex("\\s+")).last().toInt()
     val fls = lines[5].trim().split(Regex("\\s+")).last().toInt()
 
@@ -71,21 +76,21 @@ fun parseMonkey(monkey: String): Monkey {
 
 class Monkey(
     val id: Int,
-    val items: MutableList<Int>,
+    val items: MutableList<BigInteger>,
     val op: Operation,
-    val tst: Int,
+    val tst: BigInteger,
     val tBranch: Int,
     val fBranch: Int
 ) {
 
     var inspectCount = 0
 
-    fun turn(): Map<Int, List<Int>> {
+    fun turn(): Map<Int, List<BigInteger>> {
         inspectCount += items.size
 
         val result = items.map {
-            var newWorry = op.call(it) / 3
-            if (newWorry % tst == 0) {
+            var newWorry = op.call(it).divide(BigInteger.valueOf(3))
+            if (newWorry.mod(tst).compareTo(BigInteger.ZERO) == 0) {
                 tBranch to newWorry
             } else {
                 fBranch to newWorry
@@ -95,6 +100,21 @@ class Monkey(
         return result;
     }
 
+    fun turn2(input: BigInteger): Map<Int, List<BigInteger>> {
+        inspectCount += items.size
+
+        val result = items.map {
+            var newWorry = op.call(it) % input
+            if (newWorry.mod(tst).compareTo(BigInteger.ZERO) == 0) {
+                tBranch to newWorry
+            } else {
+                fBranch to newWorry
+            }
+        }.groupBy({p -> p.first}, { p -> p.second })
+        items.clear()
+        return result
+    }
+
     override fun toString(): String {
         return "Monkey{$items}"
     }
@@ -102,9 +122,23 @@ class Monkey(
 
 class MonkeyInTheMiddle(val monkeys: List<Monkey>) {
 
+    fun state(): List<Int> {
+        return monkeys.map { it.inspectCount }
+    }
+
     fun round() {
         monkeys.forEach {
             val turnResult = it.turn()
+            turnResult.forEach { (m, w) ->
+                monkeys[m].items.addAll(w)
+            }
+        }
+    }
+
+    fun round2() {
+        val tests = monkeys.map { it.tst }.reduce(BigInteger::times)
+        monkeys.forEach {
+            val turnResult = it.turn2(tests)
             turnResult.forEach { (m, w) ->
                 monkeys[m].items.addAll(w)
             }
--- src/test/kotlin/day11/Day11Test.kt
+++ src/test/kotlin/day11/Day11Test.kt
@@ -39,10 +39,55 @@ class Day11Test {
         val monkeys = input.lines().byEmptyLines().map { parseMonkey(it) }
         val middle = MonkeyInTheMiddle(monkeys)
 
-        middle.round()
+        repeat(20) { middle.round() }
+        assertEquals(listOf(101, 95, 7, 105), middle.state())
+    }
 
+    @Test
+    fun testPart2() {
+        val input = "Monkey 0:\n" +
+                "  Starting items: 79, 98\n" +
+                "  Operation: new = old * 19\n" +
+                "  Test: divisible by 23\n" +
+                "    If true: throw to monkey 2\n" +
+                "    If false: throw to monkey 3\n" +
+                "\n" +
+                "Monkey 1:\n" +
+                "  Starting items: 54, 65, 75, 74\n" +
+                "  Operation: new = old + 6\n" +
+                "  Test: divisible by 19\n" +
+                "    If true: throw to monkey 2\n" +
+                "    If false: throw to monkey 0\n" +
+                "\n" +
+                "Monkey 2:\n" +
+                "  Starting items: 79, 60, 97\n" +
+                "  Operation: new = old * old\n" +
+                "  Test: divisible by 13\n" +
+                "    If true: throw to monkey 1\n" +
+                "    If false: throw to monkey 3\n" +
+                "\n" +
+                "Monkey 3:\n" +
+                "  Starting items: 74\n" +
+                "  Operation: new = old + 3\n" +
+                "  Test: divisible by 17\n" +
+                "    If true: throw to monkey 0\n" +
+                "    If false: throw to monkey 1"
 
-        println()
+        val monkeys = input.lines().byEmptyLines().map { parseMonkey(it) }
+        val middle = MonkeyInTheMiddle(monkeys)
+
+        middle.round2()
+        assertEquals(listOf(2, 4, 3, 6), middle.state())
+        repeat(19) { middle.round2() }
+        assertEquals(listOf(99, 97, 8, 103), middle.state())
+        repeat(980) { middle.round2() }
+        assertEquals(listOf(5204, 4792, 199, 5192), middle.state())
+        repeat(1000) { middle.round2() } //2000
+        assertEquals(listOf(10419, 9577, 392, 10391), middle.state())
+        repeat(1000) { middle.round2() } //3000
+        assertEquals(listOf(15638 , 14358, 587, 15593), middle.state())
+        repeat(7000) { middle.round2() } // 10 000
+        assertEquals(listOf(52166, 47830, 1938, 52013), middle.state())
     }
 
 }
