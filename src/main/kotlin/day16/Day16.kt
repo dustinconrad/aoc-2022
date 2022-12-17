4f4386012bbeb53ed3f4c5025e0c872e2f0e36a3
Dustin Conrad <dustinaconrad@gmail.com>
2022-12-16 20:53:26 -0800

    Day 16 mid-refactor

--- src/main/kotlin/day16/Day16.kt
+++ src/main/kotlin/day16/Day16.kt
@@ -2,6 +2,7 @@ package day16
 
 import geometry.Coord
 import readResourceAsBufferedReader
+import kotlin.math.min
 
 fun main() {
     println("part 1: ${part1(readResourceAsBufferedReader("16_1.txt").readLines())}")
@@ -37,12 +38,38 @@ fun part2(input: List<String>, tl: Coord = 0 to 0, br: Coord = 4000000 to 400000
     TODO()
 }
 
+sealed interface Action {
+
+    fun apply(state: SearchState): SearchState
+
+}
+
+data class OpenValve(val node: String): Action {
+    override fun apply(state: SearchState): SearchState {
+        return state.copy(
+            opened = state.opened + node
+        )
+    }
+}
+
+data class Move(val from: String, val to: String): Action {
+    override fun apply(state: SearchState): SearchState {
+        return state.copy(
+            
+        )
+    }
+}
+
+data class SearchState(
+    val currNodes: List<String>,
+    val opened: Set<String>,
+    val minute: Int
+)
+
 class Tunnels(nodes: Collection<Node>) {
 
     private val graph = mutableMapOf<String, Node>()
-    private val opened = LinkedHashMap<String, Int>()
-    private val cache = mutableMapOf<Triple<String, Set<String>, Int>, Int>()
-    private var minutes = 30
+    private val cache = mutableMapOf<SearchState, Int>()
 
     init {
         nodes.forEach { addNode(it) }
@@ -52,52 +79,61 @@ class Tunnels(nodes: Collection<Node>) {
         graph[node.name] = node
     }
 
-    fun dfs(currNodeName: String): Int {
-        if (cache.containsKey(Triple(currNodeName, opened.keys, minutes))) {
-            return cache[Triple(currNodeName, opened.keys, minutes)]!!
-        }
-        if (minutes <= 0) {
-            return 0
-        }
-        // options are to spend a minute opening the current valve, then visit the children
-        // or skip opening this valve
-
-        var maxSoFar = 0
-        val currNode = graph[currNodeName]!!
-        // open current valve (if unopened)
-        if (!opened.containsKey(currNodeName) && currNode.rate > 0) {
-            opened[currNodeName] = minutes
-            val openCurrentValve = currNode.rate * (--minutes)
-            // and visit children
-            var maxChild = 0
-            for (child in currNode.neighbors) {
-                // travel
-                minutes--
-                val result = dfs(child)
-                // backtrack
-                minutes++
-                maxChild = maxOf(maxChild, result)
-            }
-            maxSoFar = openCurrentValve + maxChild
-            // backtrack and don't open current valve
-            minutes++
-            opened.remove(currNodeName)
-        }
-
-        var maxChild = 0
-        for (child in currNode.neighbors) {
-            // travel
-            minutes--
-            val result = dfs(child)
-            // backtrack
-            minutes++
-            maxChild = maxOf(maxChild, result)
-        }
-        // compute max
-        val max = maxOf(maxSoFar, maxChild)
-        cache[Triple(currNodeName, opened.keys, minutes)] = max
-
-        return max
+    fun dfs(currNodeName: String, minute: Int = 30): Int {
+        val initial = SearchState(
+            listOf(currNodeName),
+            emptySet(),
+            minute
+        )
+        return 0
     }
+
+//    fun dfs(state: SearchState): Int {
+//        if (cache.containsKey(state)) {
+//            return cache[state]!!
+//        }
+//        if (state.minute <= 0) {
+//            return 0
+//        }
+//        // options are to spend a minute opening the current valve, then visit the children
+//        // or skip opening this valve
+//
+//        var maxSoFar = 0
+//        val currNode = graph[currNodeName]!!
+//        // open current valve (if unopened)
+//        if (!opened.containsKey(currNodeName) && currNode.rate > 0) {
+//            opened[currNodeName] = minutes
+//            val openCurrentValve = currNode.rate * (--minutes)
+//            // and visit children
+//            var maxChild = 0
+//            for (child in currNode.neighbors) {
+//                // travel
+//                minutes--
+//                val result = dfs(child)
+//                // backtrack
+//                minutes++
+//                maxChild = maxOf(maxChild, result)
+//            }
+//            maxSoFar = openCurrentValve + maxChild
+//            // backtrack and don't open current valve
+//            minutes++
+//            opened.remove(currNodeName)
+//        }
+//
+//        var maxChild = 0
+//        for (child in currNode.neighbors) {
+//            // travel
+//            minutes--
+//            val result = dfs(child)
+//            // backtrack
+//            minutes++
+//            maxChild = maxOf(maxChild, result)
+//        }
+//        // compute max
+//        val max = maxOf(maxSoFar, maxChild)
+//        cache[Triple(currNodeName, opened.keys, minutes)] = max
+//
+//        return max
+//    }
 
 }
